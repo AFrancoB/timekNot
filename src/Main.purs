c74301ca8e1db428fea83a8711790362c4b0efe6
Alejandro <alejandro@debian-BULLSEYE-live-builder-AMD64>
2022-09-11 19:41:24 -0400

    lang ready for estuary test

--- src/Main.purs
+++ src/Main.purs
@@ -33,46 +33,44 @@ import Data.Enum
 
 import Data.Newtype
 
+import Unleash
+
 import Parsing
 
-import AST
-import Rhythmic
-import Motor
 
-
-launch :: Effect TimekNot
+launch :: Effect Unleash
 launch = do
   log "timekNot-CU: launch"
-  ast <- new $ Passage (Onsets $ L.fromFoldable []) (L.fromFoldable []) Origin true
+  ast <- new $ P (Tuple "" 0) $ Q 0.0 0.0
   tempo <- newTempo (1 % 1) >>= new 
   eval <- nowDateTime >>= new
   pure { ast, tempo, eval}  
 
-evaluate :: TimekNot -> String -> Effect { success :: Boolean, error :: String }
-evaluate timekNot str = do
-  log "timekNot-CU: evaluate"
-  passage <- read timekNot.ast
+evaluate :: Unleash -> String -> Effect { success :: Boolean, error :: String }
+evaluate unleash str = do
+  log "Unleash-Windsor: evaluate"
+  program <- read unleash.ast
  -- log $ show passage
   -- placeholder: assume any evaluation yields the valid program
   eval <- nowDateTime
-  let pr = pErrorToString $ runParser str topPassageParser -- :: Either String AST 
+  let pr = pErrorToString $ runParser str parseSample -- :: Either String AST 
   case pr of
     Left error -> pure $ { success: false, error }
     Right p -> do
-      write eval timekNot.eval --- esto!!!!
-      write p timekNot.ast 
+      write eval unleash.eval --- esto!!!!
+      write p unleash.ast 
       pure $ { success: true, error: "" }    --- here you might add the eval time with purescript. 
 
-pErrorToString:: Either ParseError Passage -> Either String Passage
+pErrorToString:: Either ParseError Program -> Either String Program
 pErrorToString (Left x) = Left $ parseErrorMessage x
 pErrorToString (Right x) = Right x
 
-setTempo :: TimekNot -> ForeignTempo -> Effect Unit
-setTempo timekNot t = write (fromForeignTempo t) timekNot.tempo
+setTempo :: Unleash -> ForeignTempo -> Effect Unit
+setTempo unleash t = write (fromForeignTempo t) unleash.tempo
 
 -- here a func that goes from rhythmicInto (passing through map) Event
-scheduleNoteEvents :: TimekNot -> Number -> Number -> forall opts. Effect (Array Foreign)
-scheduleNoteEvents tk ws we = timekNotToForeigns tk ws we
+scheduleNoteEvents :: Unleash -> Number -> Number -> forall opts. Effect (Array Foreign)
+scheduleNoteEvents tk ws we = unleashToForeigns tk ws we
 
 -- make unsafe function and correct with david's advice later
 numToDateTime:: Number -> DateTime 
@@ -85,41 +83,23 @@ unsafeMaybeMilliseconds:: Maybe Instant -> Instant
 unsafeMaybeMilliseconds (Just x) = x
 unsafeMaybeMilliseconds Nothing = unsafeMaybeMilliseconds $ instant $ Milliseconds 0.0
 
-timekNotToForeigns:: TimekNot -> Number -> Number -> forall opts. Effect (Array Foreign)
-timekNotToForeigns tk ws we = do
+unleashToForeigns:: Unleash -> Number -> Number -> forall opts. Effect (Array Foreign)
+unleashToForeigns un ws we = do
     let ws' = numToDateTime (ws * 1000.0000)
     let we' = numToDateTime (we * 1000.0000)
-    passage <- read tk.ast
-    t <- read tk.tempo
-    eval <- read tk.eval
+    program <- read un.ast
+    t <- read un.tempo
+    eval <- read un.eval
 
-    log $ show passage
+    log $ show program
     log $ show ws
     log $ show we
-    let events = fromPassageToArray passage t ws' we' eval
+
+    let events = fromPassageToArray program t ws' we' eval
     log $ show events
     pure $ map unsafeToForeign events
 
-fromPassageToArray:: Passage -> Tempo -> DateTime -> DateTime -> DateTime -> Array {whenPosix:: Number, s:: String, n:: Int}
-fromPassageToArray pass t ws we eval = 
-    let events' = passageToEvents pass t ws we eval -- List (Maybe Event)
-        events = toUnfoldable $ filterMaybe events' -- Array
-      in events
+fromPassageToArray:: Program -> Tempo -> DateTime -> DateTime -> DateTime -> Array {whenPosix:: Number, s:: String, n:: Int}
+fromPassageToArray prog t ws we eval = [actualise prog t eval]
 
 
-filterMaybe:: List (Maybe Event) -> List Event
-filterMaybe x = map withoutMaybe $ filter justJust x
-
-withoutMaybe:: Maybe Event -> Event
-withoutMaybe (Just x) = x
-withoutMaybe Nothing = {whenPosix: 0.0, s: "", n: 0}
-
-justJust:: Maybe Event -> Boolean
-justJust (Just _) = true
-justJust Nothing = false
-
-coordToEvent:: Coordenada -> {whenPosix:: Number, s:: String, n:: Int}
-coordToEvent (Coord num iEv iPas) = {whenPosix: num, s: "cp", n: 0 }
-
-testMaybeInstant:: Number -> Maybe Instant
-testMaybeInstant x = instant $ Milliseconds x -- Maybe Instant
---
+++ src/Unleash.purs
@@ -0,0 +1,178 @@
+module Unleash (parseSample,actualise,Program(..),Quant(..),Unleash(..),t,eval,evalTimeandQuantToPsx) where
+
+import Prelude
+
+import Data.Either
+import Data.Identity
+import Data.List hiding (many)
+import Data.Foldable (foldl)
+import Data.Int
+import Data.Tuple
+import Data.String (singleton, joinWith, take, split, trim, Pattern(..))
+import Data.Maybe hiding (optional)
+import Data.Functor
+import Control.Monad
+
+import Effect (Effect)
+import Effect.Console (log)
+import Effect.Ref
+
+import Parsing
+import Parsing.String
+import Parsing.String.Basic
+import Parsing.Combinators
+import Parsing.Language (haskellStyle)
+import Parsing.Token (makeTokenParser)
+
+import Data.DateTime
+import Data.DateTime.Instant
+import Data.Time.Duration
+import Data.Tempo
+import Data.Enum
+import Partial.Unsafe
+
+import Data.Newtype
+
+import Data.Rational hiding (toNumber)
+
+type P = ParserT String Identity
+
+parseSample:: P Program
+parseSample = do
+    _ <- pure 1
+    x <- sampler
+    _ <- string "||"
+    whitespace
+    q <- parseQuant
+    eof
+    pure $ P x q
+
+sampler:: P (Tuple String Int)
+sampler = do
+    _ <- pure 1
+    x <- stringLit
+    whitespace
+    pure $ stringToSamples' x
+
+stringToSamples':: String -> Tuple String Int -- what to do with commas??
+stringToSamples' s = 
+    let xs = fromFoldable $ split (Pattern ":") $ trim s
+        name = fromMaybe " " $ head xs
+        n = fromMaybe 0 $ fromTailToMaybeInt xs
+    in Tuple name n
+
+
+fromTailToMaybeInt :: List String -> Maybe Int
+fromTailToMaybeInt strs = do
+  a <- tail strs -- String -> Maybe (List String)
+  b <- head a
+  c <- fromString b -- Maybe Int
+  pure c
+
+parseQuant:: P Quant
+parseQuant = do
+    _ <- pure 1
+    _ <- string "quant"
+    whitespace
+    x <- naturalOrFloat
+    y <-naturalOrFloat <|> pure (Right 0.85)
+    pure $ Q (asNumber x) (asNumber y)
+
+asNumber:: Either Int Number -> Number
+asNumber (Left x) = toNumber x
+asNumber (Right x) = x
+
+
+---
+actualise:: Program -> Tempo -> DateTime -> {whenPosix:: Number, s:: String, n:: Int}
+actualise (P sn q) t eval = singleVirtualToActual sn q t eval
+
+singleVirtualToActual:: (Tuple String Int) -> Quant -> Tempo -> DateTime -> {whenPosix:: Number, s:: String, n:: Int}
+singleVirtualToActual (Tuple sample ene) (Q quant calibrate) t eval = {whenPosix: psx, s: sample, n: ene}
+    where psx = fromDateTimeToPosix $ evalTimeandQuantToPsx t eval quant
+
+evalTimeandQuantToPsx:: Tempo -> DateTime -> Number -> DateTime
+evalTimeandQuantToPsx t eval q = 
+    let cuenta = timeToCountNumber t eval
+        calibrated = if (justFractional cuenta) <= 0.85 then cuenta else (cuenta + 5.0)
+        cuentaInGrid = toNumber $ ceil cuenta
+        cuentaQ = cuentaInGrid + q 
+        posInTempo = (toRat cuentaQ)
+    in countToTime t posInTempo
+
+fromDateTimeToPosix:: DateTime -> Number
+fromDateTimeToPosix x = (unwrap $ unInstant $ fromDateTime x)/1000.0000
+
+toRat:: Number -> Rational
+toRat x = 
+    let pFact = 1000000
+        floored = floor x -- 12
+        fract = x - (toNumber floored) -- 12.5 - 12.0 = 0.5
+        fract' = round $ fract * (toNumber pFact) -- 500000
+    in (floored%1) + (fract'%pFact) -- 12 + (500000%1000000)
+
+
+justFractional:: Number -> Number
+justFractional x = x - (toNumber $ floor x)
+
+-----
+
+data Program = P (Tuple String Int) Quant 
+
+instance Show Program where
+    show (P (Tuple s n) q) = show s <> " " <> show n <> " " <> show q
+
+data Quant = Q Number Number
+
+instance Show Quant where
+    show (Q quant calibrate) = show quant <> " " <> show calibrate
+
+-----
+
+tokenParser = makeTokenParser haskellStyle
+parens      = tokenParser.parens
+braces      = tokenParser.braces
+identifier  = tokenParser.identifier
+reserved    = tokenParser.reserved
+naturalOrFloat = tokenParser.naturalOrFloat
+float = tokenParser.float
+whitespace = tokenParser.whiteSpace
+colon = tokenParser.colon
+brackets = tokenParser.brackets
+comma = tokenParser.comma
+semi = tokenParser.semi
+integer = tokenParser.integer
+stringLit = tokenParser.stringLiteral
+
+----
+makeDate :: Int -> Month -> Int -> Date
+makeDate y m d = 
+    unsafePartial $ fromJust $ 
+       canonicalDate <$> toEnum y <@> m <*> toEnum d
+
+makeTime :: Int -> Int -> Int -> Int -> Time
+makeTime h min sec milisec = 
+    unsafePartial $ fromJust $ Time <$> toEnum h <*> toEnum min <*> toEnum sec <*> toEnum milisec
+
+t:: Tempo
+t = {freq: (2%1),time: (DateTime (makeDate 2022 June 3) (makeTime 19 11 25 100)), count: fromInt 0 }
+
+tAncient:: Tempo
+tAncient = {freq: (2%1),time: (DateTime (makeDate 2012 June 3) (makeTime 19 11 25 100)), count: fromInt 0 }
+
+ws:: Int -> Int -> DateTime
+ws x y = (DateTime (makeDate 2022 June 3) (makeTime 19 15 x y))
+
+we:: Int -> Int -> DateTime
+we x y = (DateTime (makeDate 2022 June 3) (makeTime 19 15 x y))
+
+eval:: DateTime
+eval = (DateTime (makeDate 2022 June 3) (makeTime 19 15 0 485))
+
+---
+
+type Unleash = {
+  ast :: Ref Program,
+  tempo :: Ref Tempo,
+  eval :: Ref DateTime
+  }
